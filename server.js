7f399b71eb2824f824e194c20c3d27f6e38f21bd
Yaniv Shulman <yashulman@gmail.com>
2014-12-23 17:47:46 +0200

    Links draft

--- server.js
+++ server.js
@@ -2,15 +2,55 @@ var request = require('request');
 var cheerio = require('cheerio');
 var phantom = require('phantom');
 var http = require('http');
-var Dropbox = require('dropbox');
 
 var settings = require('./settings.json');
 var mainURL = settings.mainURL;
-var playerURLPrefix = settings.playerURLPrefix;
-var sourceFilePrefix = settings.sourceFilePrefix;
-var localPath = settings.localPath;
-var tmpPath = settings.tmpPath;
-var destinationPath = settings.destinationPath;
+var area = settings.preferences.area;
+var city = settings.preferences.city;
+var neighborhood = settings.preferences.neighborhood;
+var type = settings.preferences.type;
+var fromRooms = settings.preferences.fromRooms;
+var untilRooms = settings.preferences.untilRooms;
+var fromPrice = settings.preferences.fromPrice;
+var topPrice = settings.preferences.topPrice;
+var priceType = settings.preferences.priceType;
+var fromSquareMeter = settings.preferences.fromSquareMeter;
+var topSquareMeter = settings.preferences.topSquareMeter;
+var parking = settings.preferences.parking;
+var elevator = settings.preferences.elevator;
+var fromFloor = settings.preferences.fromFloor;
+var topFloor = settings.preferences.topFloor;
+var streetId = settings.preferences.street;
+
+
+var curURI = "";
+
+for (i in settings.streets) {
+
+    curURI =  mainURL + "?" +
+    area[0] + "=" + area[1] + "&" +
+    city[0] + "=" + encodeHeb(city[1]) + "&" +
+    neighborhood[0] + "=" + neighborhood[1] + "&" +
+    fromRooms[0] + "=" + fromRooms[1] + "&" +
+    untilRooms[0] + "=" + untilRooms[1] + "&" +
+    fromPrice[0] + "=" + fromPrice[1] + "&" +
+    topPrice[0] + "=" + topPrice[1] + "&" +
+    priceType[0] + "=" + priceType[1] + "&" +
+    fromSquareMeter[0] + "=" + fromSquareMeter[1] + "&" +
+    topSquareMeter[0] + "=" + topSquareMeter[1] + "&" +
+    parking[0] + "=" + parking[1] + "&" +
+    elevator[0] + "=" + elevator[1] + "&" +
+    fromFloor[0] + "=" + fromFloor[1] + "&" +
+    topFloor[0] + "=" + topFloor[1] + "&" +
+    streetId[0] + "=" + encodeHeb(settings.streets[i]);
+
+    console.log(curURI);
+
+
+};
+
+/*
+
 
 var latestDownloadDetailsFile = 'latest-download.json';
 var latestDownloadDetails = { fileName : "", path : tmpPath, playerURL : ""};
@@ -138,3 +178,36 @@ function saveHTML(fileName, html) {
   });
 
 };
+*/
+
+function encodeHeb(q){
+  q=q.replace(/א/gi,"%E0");
+  q=q.replace(/ב/gi,"%E1");
+  q=q.replace(/ג/gi,"%E2");
+  q=q.replace(/ד/gi,"%E3");
+  q=q.replace(/ה/gi,"%E4");
+  q=q.replace(/ו/gi,"%E5");
+  q=q.replace(/ז/gi,"%E6");
+  q=q.replace(/ח/gi,"%E7");
+  q=q.replace(/ט/gi,"%E8");
+  q=q.replace(/י/gi,"%E9");
+  q=q.replace(/כ/gi,"%EB");
+  q=q.replace(/ך/gi,"%EA");
+  q=q.replace(/ל/gi,"%EC");
+  q=q.replace(/מ/gi,"%EE");
+  q=q.replace(/ם/gi,"%ED");
+  q=q.replace(/נ/gi,"%F0");
+  q=q.replace(/ן/gi,"%EF");
+  q=q.replace(/ס/gi,"%F1");
+  q=q.replace(/ע/gi,"%F2");
+  q=q.replace(/פ/gi,"%F4");
+  q=q.replace(/ף/gi,"%F3");
+  q=q.replace(/צ/gi,"%F6");
+  q=q.replace(/ץ/gi,"%F5");
+  q=q.replace(/ק/gi,"%F7");
+  q=q.replace(/ר/gi,"%F8");
+  q=q.replace(/ש/gi,"%F9");
+  q=q.replace(/ת/gi,"%FA");
+  q=q.replace(/ /gi,"+");
+  return (q);
+}
